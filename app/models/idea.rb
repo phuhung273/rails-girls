6e28b5bea1a0cb61783db358ac948b0409d2bf12
phuhung273 <hung.tran@alttekglobal.com>
2020-11-25 17:40:56 +0700

    add comments

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,7 @@
 class Idea < ApplicationRecord
 
+    has_many :comments
+
     mount_uploader :picture, PictureUploader
-    
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_093526) do
+ActiveRecord::Schema.define(version: 2020_11_25_102855) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
